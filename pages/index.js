ed718a45f8887a33dfeb06103fdf7e0bce57373a
Siiickzer <suphanath_w@cmu.ac.th>
2022-08-18 16:17:46 +0700

    finish

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,76 @@ import {
   IconArrowUp,
   IconArrowDown,
 } from "@tabler/icons";
-
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [todoText, setTodotext] = useState([]);
 
-  const markTodo = (idx) => {};
+  const deleteTodo = (idx) => {
+    todoText.splice(idx, 1);
+    const newTodos = [...todoText];
+    setTodotext(newTodos);
+  };
 
-  const moveUp = (idx) => {};
+  const markTodo = (idx) => {
+    todoText[idx].completed = !todoText[idx].completed;
+    setTodotext([...todoText]);
+  };
 
-  const moveDown = (idx) => {};
+  const moveUp = (idx) => {
+    if (idx === 0) {
+      return;
+    } else {
+      const temp = todoText[idx];
+      todoText[idx] = todoText[idx - 1];
+      todoText[idx - 1] = temp;
+      setTodotext([...todoText]);
+    }
+  };
+
+  const moveDown = (idx) => {
+    if (idx === todoText.length - 1) {
+      return;
+    } else {
+      const temp = todoText[idx];
+      todoText[idx] = todoText[idx + 1];
+      todoText[idx + 1] = temp;
+      setTodotext([...todoText]);
+    }
+  };
+
+  const addTodo = (title, completed) => {
+    setTodotext([{ title: title, completed: completed }, ...todoText]);
+  };
+
+  const onKeyUpHandler = (event) => {
+    if (event.key !== "Enter") return;
+    if (event.target.value === "") {
+      alert("Todo can not be empty");
+      return;
+    }
+    addTodo(event.target.value, false);
+    event.target.value = "";
+  };
+
+  useEffect(() => {
+    const todosStr = localStorage.getItem("react-todos");
+    if (!todosStr) setTodotext([]);
+    else setTodotext(JSON.parse(todosStr));
+  }, []);
+
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    saveTodos();
+  }, [todoText]);
+
+  const saveTodos = () => {
+    const todosStr = JSON.stringify(todoText);
+    localStorage.setItem("react-todos", todosStr);
+  };
 
   return (
     <div>
@@ -28,40 +89,33 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyUp={onKeyUpHandler}
         />
-        {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
-
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
-
+        {todoText.map((Element, i) => (
+          <Todo
+            title={Element.title}
+            completed={Element.completed}
+            key={i}
+            onMoveUp={() => moveUp(i)}
+            onMoveDown={() => moveDown(i)}
+            onDelete={() => deleteTodo(i)}
+            onMark={() => markTodo(i)}
+          />
+        ))}
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({todoText.length}) </span>
+          <span className="text-warning">
+            Pending ({todoText.filter((Ele) => Ele.completed == false).length})
+          </span>
+          <span className="text-success">
+            Completed ({todoText.filter((Ele) => Ele.completed == true).length})
+          </span>
         </p>
 
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Suphanath Wangthip 640612098
         </p>
       </div>
     </div>
